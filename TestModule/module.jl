703ea31bad9fc1e4d80641f00b3005adcaf81a59
chdlkl <chdlkl@163.com>
2018-08-02 22:47:35 +0800

    update

---
+++ TestModule/module.jl
@@ -0,0 +1,10 @@
+module MyModule
+
+export my, he, f
+my = 123
+he = 24
+function f( x, y )
+    ans = x + y
+end
+
+end
---
+++ TestModule/test.jl
@@ -0,0 +1,6 @@
+include( "module.jl" )
+using MyModule
+
+println( "my = ", my )
+println( "he = ", he )
+println( "the result of fun is ", f(22,23) )
